43a82b7f699ecf6b6dfc98ab2b2f60ac831f7cbb
可樂呦 <145786257+Colayo0103@users.noreply.github.com>
2023-12-26 13:08:23 +0800

    Add files via upload

---
+++ Ex13_02.java
@@ -0,0 +1,12 @@
+public class Ex13_02{
+    public static void main(String[] args){
+        int num=12,den=0;
+        if(den==0){
+            System.out.println("除數為0"); 
+        }
+        else{
+            int ans=num/den;
+            System.out.println(ans); 
+        }
+    }
+}
---
+++ Ex13_03.java
@@ -0,0 +1,12 @@
+public class Ex13_03{
+    public static void main(String[] args){
+        try{
+            int num=12,den=0;
+            int ans=num/den;
+            System.out.println(ans); 
+        }
+        catch(ArithmeticException e){
+            System.out.println("除數為0");
+        }
+    }
+}
